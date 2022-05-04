a860da592d5bb9d4bb95169c2fcfd91359306dae
hexafluoride <xafluoride@gmail.com>
2022-05-04 11:35:49 -0500

    Fix balance page error/empty default tokens list

--- Shadena/SettingsModel.cs
+++ Shadena/SettingsModel.cs
@@ -7,7 +7,7 @@ public class SettingsModel
     public Network Network { get; set; } = Network.Testnet;
     public string CustomNetworkEndpoint { get; set; }
     public string CustomNetworkId { get; set; }
-    public List<string> Tokens { get; set; }
+    public List<string> Tokens { get; set; } = new() { "coin" };
 
     public PactClientSettings GenerateClientSettings()
     {
